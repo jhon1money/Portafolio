d7e12e6baaa366dc30973121c91548e537b599b8
jhon1money <120067843+jhon1money@users.noreply.github.com>
2024-03-02 19:05:47 -0400

    Portafolio

---
+++ main.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, send_file, request, flash
+from email.message import EmailMessage
+import ssl
+import smtplib
+
+app = Flask(__name__)
+
+app.secret_key = "IDONTKNOW"
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/send_message', methods=['POST'])
+def send_message():
+    if request.method == 'POST':
+        name = request.form['name']
+        phone = request.form['phone']
+        email_from = request.form['email']
+        subject = request.form['subject']
+        message = request.form['message']
+
+        email_receiver = 'jhonanthonyjimenez568@gmail.com'
+        email_password = 'gkip nqmh wkit agmb'
+
+        contact_email = EmailMessage()
+        contact_email["From"] = email_from
+        contact_email["To"] = email_receiver
+        contact_email["Subject"] = subject
+        contact_email.set_content(f"Name: {name}\nPhone: {phone}\nMessage: {message}")
+
+        context = ssl.create_default_context()
+
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+            smtp.login(email_receiver, email_password)
+            smtp.send_message(contact_email)
+        flash(f"Thank so much for {name} contanct me I'll be more than glad to reach back a'")
+        return "Email sent successfully!"
+
+@app.route('/download_cv')
+def download_cv():
+    pdf_path = "C:/Users/18097/Desktop/Portafolio/curriculum.pdf"
+    return send_file(pdf_path, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
